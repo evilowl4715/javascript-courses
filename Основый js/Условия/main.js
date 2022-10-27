76e4197a815a8b33db45c7068e80f276a960c20d
Xenia <evilowl4715@gmail.com>
2022-10-27 09:07:17 +0400

    Условия

---
+++ Основый js/Условия/main.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверо');
+        break;
+    case 100:
+        console.log('Неверо');
+        break;
+    case 50:
+        console.log('Верно');
+        break;
+    default:
+        console.log('Не сегодня');
+        break;
+}
