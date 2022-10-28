560dcf0a9d96eb862b74500ff9839332d52aa78d
Xenia <evilowl4715@gmail.com>
2022-10-28 09:36:25 +0400

    Задачи по циклам

--- Основый js/Циклы/main.js
+++ Основый js/Циклы/main.js
@@ -55,3 +55,80 @@ first: for (let i = 0; i < 3; i++) {
         }
     }
 }
+
+
+function firstTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+
+    // Пишем решение вот тут
+    
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = arr[i];
+    }
+    
+    console.log(result);
+
+    // Не трогаем
+    return result;
+}
+
+firstTask();
+
+// Место для второй задачи
+function secondTask() {
+    // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+    let data = [5, 10, 'Shopping', 20, 'Homework'];
+
+    // Пишем решение вот тут
+
+        for (let i = 0; i < data.length; i++) {
+        
+            if(typeof(data[i]) === 'string') {
+                data[i] += " - done";
+            } else if(typeof(data[i]) === 'number') {
+               data[i] = data[i] * 2;
+            }
+    }
+    
+    console.log(data);
+    
+    
+    // Не трогаем
+    return data;
+}
+secondTask();
+
+
+function thirdTask() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+
+    for (let i = 1; i <= data.length; i++) {
+        result[i - 1] = data[data.length - i]
+    }
+
+    console.log(result);
+    return result;
+}
+
+thirdTask();
+
+
+
+
+const lines = 5;
+let result = '';
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += " ";
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += "*";
+    }
+    result += "\n";
+}
+
+console.log(result);
