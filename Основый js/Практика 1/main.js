970a4fba581b33e0b537df52f3d1f5940bf60cfe
Xenia <evilowl4715@gmail.com>
2022-10-28 10:07:18 +0400

    Практика ч2

--- Основый js/Практика 1/main.js
+++ Основый js/Практика 1/main.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-const ratingFilm = prompt('На сколько оцените его?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
@@ -11,6 +9,27 @@ const personalMovieDB = {
     privat: false
 };
 
-personalMovieDB.movies[lastFilm] = ratingFilm;
+for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+    const ratingFilm = prompt('На сколько оцените его?', '');
+
+    if (lastFilm != null && ratingFilm != null && lastFilm != '' && ratingFilm != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = ratingFilm;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Ошибка');
+}
 
 console.log(personalMovieDB);
