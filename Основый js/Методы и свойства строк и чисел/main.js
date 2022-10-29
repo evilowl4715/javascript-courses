aacf5510bdddbb2db8f89efac4778edc72b54a45
Xenia <evilowl4715@gmail.com>
2022-10-29 06:42:09 +0400

    Методы и свойства строк и чисел

---
+++ Основый js/Методы и свойства строк и чисел/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 4];
+
+// console.log(str[2]);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+let fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "hello world";
+// console.log(logg.slice(6));
+// console.log(logg.slice(6, 11));
+// console.log(logg.substring(6, 11));
+// console.log(logg.substr(6, 5));
+
+const num = 12.2;
+const test = '12.2px';
+
+// console.log(Math.round(num));
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
+// console.dir(Number);
