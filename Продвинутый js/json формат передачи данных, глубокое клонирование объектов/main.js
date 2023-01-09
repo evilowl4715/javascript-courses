a9d7a4d0b6fc60ee97467bc6ee8cb6160540d6a9
Xenia <evilowl4715@gmail.com>
2023-01-09 06:42:24 +0300

    json формат передачи данных, глубокое клонирование объектов

---
+++ Продвинутый js/json формат передачи данных, глубокое клонирование объектов/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const persone = {
+    name: 'Alex',
+    tel: '+75555',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
+};
+
+const clone = JSON.parse(JSON.stringify(persone));
+clone.parents.mom = 'Ann';
+console.log(persone);
+console.log(clone);
